a6d761956c384fa9e30e75742fdc3fcb4824e970
saaeroos <mh.mxxm@gmail.com>
2026-03-18 10:05:52 +0100

    create a utility folder

---
+++ frontend/src/utils/cn.ts
@@ -0,0 +1,12 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * Utility function to conditionally join class names using clsx and tailwind-merge.
+ * @param {...ClassValue[]} inputs - Class values to be conditionally joined.
+ * @returns {string} - The merged class names.
+ */
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
